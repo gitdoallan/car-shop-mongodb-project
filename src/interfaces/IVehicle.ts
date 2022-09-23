1a84b33f8e3f4a6898e59584fdff191de48c192d
gitdoallan <mailtoallan@icloud.com>
2022-09-23 05:23:10 -0300

    feat: IVehicle interface

---
+++ src/interfaces/IVehicle.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+
+const schema = z.object({
+  model: z.string().min(3),
+  year: z.number().int().gt(1900).lte(2022),
+  color: z.string().min(3),
+  status: z.boolean().optional(),
+  buyValue: z.number().int(),
+});
+
+export type IVehicle = z.infer<typeof schema>;
